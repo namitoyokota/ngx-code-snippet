420d304e74d7b51dd464899e5c2f36e7df92d04d
Namito Yokota <namitoyokota@icloud.com>
2024-05-04 10:22:22 -0500

    Fixed package dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CodeSnippetModule } from 'ngx-code-snippet';
-import { HighlightModule } from 'ngx-highlightjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, CodeSnippetModule, HighlightModule],
+    imports: [BrowserModule, CodeSnippetModule],
     providers: [],
     bootstrap: [AppComponent],
 })
